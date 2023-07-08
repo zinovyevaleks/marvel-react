fa87ecd1f05a4f6574dc92c94eac0fa275ecb544
Aleksandr Zinovyev <zinovyevaleks@yandex.ru>
2023-07-08 10:32:38 +0700

    Character info

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,9 @@
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+
+import decoration from '../../resources/img/vision.png';
 
 const App = () => {
   return (
@@ -10,7 +13,9 @@ const App = () => {
         <RandomChar />
         <div className="char__content">
           <CharList />
+          <CharInfo />
         </div>
+        <img className="bg-decoration" src={decoration} alt="vision" />
       </main>
     </div>
   )
