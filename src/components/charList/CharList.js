f2b0274733263360666c3bbe56618be7fd99476b
Aleksandr Zinovyev <zinovyevaleks@yandex.ru>
2023-11-13 18:58:52 +0700

    Add loading of new items by clicking Load more

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -9,23 +9,44 @@ class CharList extends Component {
   state = {
     charList: [],
     loading: true,
-    error: false
+    error: false,
+    newItemLoading: false,
+    offset: 210,
+    charEnded: false
   }
 
   marvelService = new MarvelService();
 
   componentDidMount() {
-    this.marvelService
-        .getAllCharacters()
-        .then(this.onCharListLoaded)
-        .catch(this.onError)
+    this.onRequest();
   }
 
-  onCharListLoaded = (charList) => {
+  onRequest = (offset) => {
+    this.onCharListLoading();
+    this.marvelService.getAllCharacters(offset)
+      .then(this.onCharListLoaded)
+      .catch(this.onError)
+  }
+
+  onCharListLoading = () => {
     this.setState({
-      charList,
-      loading: false
+      newItemLoading: true
     })
+  }
+
+  onCharListLoaded = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+
+    this.setState(({offset, charList}) => ({
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+      charEnded: ended
+    }))
   }
 
   onError = () => {
@@ -35,35 +56,34 @@ class CharList extends Component {
     })
   }
 
-renderItems(arr) {
-  const items =  arr.map((item) => {
-    let imgStyle = {'objectFit' : 'cover'};
-    if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
-        imgStyle = {'objectFit' : 'unset'};
-    }
+  renderItems(arr) {
+    const items =  arr.map((item) => {
+      let imgStyle = {'objectFit' : 'cover'};
+      if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+          imgStyle = {'objectFit' : 'unset'};
+      }
+
+      return (
+        <li
+          className="char__item"
+          key={item.id}
+          onClick={() => this.props.onCharSelected(item.id)}>
+            <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
+            <div className="char__name">{item.name}</div>
+        </li>
+      )
+    });
 
     return (
-      <li
-        key={item.id}
-        className="char__item"
-        onClick={() => this.props.onCharacterSelected(item.id)}>
-          <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
-          <div className="char__name">{item.name}</div>
-      </li>
-    )
-  });
-
-  return (
       <ul className="char__grid">
           {items}
       </ul>
-  )
-}
+    )
+  }
 
   render() {
-    const {charList, loading, error} = this.state;
+    const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
     const items = this.renderItems(charList);
-
     const errorMessage = error ? <ErrorMessage/> : null;
     const spinner = loading ? <Spinner/> : null;
     const content = !(loading || error) ? items : null;
@@ -73,7 +93,11 @@ renderItems(arr) {
         {errorMessage}
         {spinner}
         {content}
-        <button className="button button__main button__long">
+        <button
+          className="button button__main button__long"
+          disabled={newItemLoading}
+          style={{'display': charEnded ? 'none' : 'block'}}
+          onClick={() => this.onRequest(offset)}>
           <div className="inner">load more</div>
         </button>
       </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = 'https://gateway.marvel.com:443/v1/public/';
-  _apiKey = 'apikey=40f8f96384ddc910523218361b21b5ad'
+  _apiKey = 'apikey=40f8f96384ddc910523218361b21b5ad';
+  _baseOffset = 210;
 
   getResource = async (url) => {
     let resource = await fetch(url);
@@ -12,9 +13,9 @@ class MarvelService {
     return await resource.json();
   }
 
-  getAllCharacters = async () => {
-    const result = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`)
-    return result.data.results.map(this._transformCharacter);
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
   }
 
   getCharacter = async (id) => {
