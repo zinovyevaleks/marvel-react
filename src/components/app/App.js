1a9814df9b40e258affcaf0c79be6442d7c5737a
Aleksandr Zinovyev <zinovyevaleks@yandex.ru>
2023-12-29 17:16:37 +0700

    add routes to pages

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,37 +1,25 @@
-import { useState } from 'react';
-import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-import decoration from '../../resources/img/vision.png';
+import { MainPage, ComicsPage } from '../pages';
+import AppHeader from "../appHeader/AppHeader";
 
 const App = () => {
-  const [selectedCharacter, setSelectedCharacter] = useState(null);;
-
-  const onCharacterSelected = (id) => {
-    setSelectedCharacter(id);
-  }
-
   return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        {/* <RandomChar />
-        <div className="char__content">
-          <CharList onCharacterSelected={onCharacterSelected}/>
-          <ErrorBoundary>
-            <CharInfo characterId={selectedCharacter}/>
-          </ErrorBoundary>
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" /> */}
-        <AppBanner/>
-        <ComicsList/>
-      </main>
-    </div>
+    <Router>
+      <div className="app">
+        <AppHeader />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <MainPage/>
+            </Route>
+            <Route exact path="/comics">
+              <ComicsPage/>
+            </Route>
+          </Switch>
+        </main>
+      </div>
+    </Router>
   )
 }
 
